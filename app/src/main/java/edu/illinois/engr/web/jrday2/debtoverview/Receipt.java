30fcab09d592fc1f07eab842f61df1485f8f17b2
Finkoy <Finkoy@github.com>
2016-02-20 23:14:48 -0600

    Working Menu Maybe Working Camera Working Camera, idk how to store data

--- app/src/main/java/edu/illinois/engr/web/jrday2/debtoverview/Receipt.java
+++ app/src/main/java/edu/illinois/engr/web/jrday2/debtoverview/Receipt.java
@@ -1,7 +1,10 @@
 package edu.illinois.engr.web.jrday2.debtoverview;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 /**
@@ -26,24 +30,28 @@ public class Receipt extends Fragment
     static final int CAM_REQUEST = 1;
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        if(container == null) return null;
         rootview = inflater.inflate(R.layout.receipt, container, false);
-        button = (Button) getActivity().findViewById(R.id.angry_btn);
-        imageView = (ImageView) getActivity().findViewById(R.id.image_view);
+
+        button = (Button) rootview.findViewById(R.id.button);
+        imageView = (ImageView) rootview.findViewById(R.id.image_view);
+
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
             {
                 Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getFile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                //File file = getFile();
+                //camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(camera_intent, CAM_REQUEST);
             }
         });
         return rootview;
     }
 
-    private File getFile()
+    /*private File getFile()
     {
         File folder = new File("sdcard/camera_app");
         if(!folder.exists())
@@ -53,11 +61,26 @@ public class Receipt extends Fragment
         File image_file = new File(folder, "cam_image.jpg");
 
         return image_file;
-    }
+    }*/
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        String path = "sdcard/camera_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAM_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                // convert byte array to Bitmap
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+                imageView.setImageBitmap(bitmap);
+
+            }
+        }
     }
 }
